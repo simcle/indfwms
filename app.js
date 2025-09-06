e947fb9c22d24cabb3df16cc2dd009ad64f7abf9
NendiSofiandy <nendisofiandy80@gmail.com>
2025-09-06 07:09:40 +0700

    upadate

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 import "dotenv/config"
+import express from 'express'
+import cors from 'cors'
+
+import path from 'path'
+import { fileURLToPath } from "url"
+
 import { connectMqtt } from "./src/mqttClient.js"
 import "./src/utils/opcClient.js"
 
 connectMqtt(process.env.MQTT_URL)
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
