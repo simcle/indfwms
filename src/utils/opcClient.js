8fae6fac1b31f94a4442c751123c360eb49527cf
NendiSofiandy <nendisofiandy80@gmail.com>
2025-09-08 09:11:20 +0700

    update

--- src/utils/opcClient.js
+++ src/utils/opcClient.js
@@ -84,6 +84,7 @@ async function main() {
             }
             latestValues[tag.name] = raw
             latestValues.timestamp = new Date().toISOString();
+            console.log(latestValues)
             publish("opc/data", latestValues)
         });
     }
